5abe10b5bd7543bd1413e176013c90271fe89309
darektoa <darektoa@gmail.com>
2022-08-22 17:11:25 +0700

    Add: Join method in string helper object

--- src/scripts/utilities/string-helper.js
+++ src/scripts/utilities/string-helper.js
@@ -1,5 +1,10 @@
 const StringHelper = {
-    
+    join(separator, ...values) {
+      if(!values.length) return '';
+      return values.reduce((tempResult, value) => (
+        !value ? tempResult : `${tempResult}${separator}${value}`
+      ));
+    },
 };
 
 export default StringHelper;
