8d1426f7ec1772a73ede589057460adf98389562
darektoa <darektoa@gmail.com>
2022-08-23 11:53:46 +0700

    Add: Using header component in App component

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,13 +1,23 @@
 import '../styles/App.css';
 import { Routes, Route } from 'react-router-dom';
 import Activity from './pages/Activity';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Activity/>} />
-    </Routes>
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
+
+const Main = () => (
+  <main>
+    <Routes>
+      <Route path="/" element={<Activity/>} />
+    </Routes> 
+  </main>
+)
 
 export default App;
