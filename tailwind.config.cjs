780e7b2b13cc51c76b0cba96c6150aafa0a9a616
darektoa <darektoa@gmail.com>
2022-08-23 11:40:13 +0700

    Add: Custom colors in tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: [ "./index.html", "./src/**/*.{html,js,jsx,ts,tsx}" ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#16ABF8',
+        danger: '#ED4C5C',
+        warning: '#F8A541',
+        success: '#00A790',
+        info: '#428BC1',
+        epic: '#8942C1',
+      }
+    },
   },
   plugins: [],
 }
