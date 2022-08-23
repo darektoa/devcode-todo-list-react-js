21f0ddaacf09ede1900a1ae37012ce0d5d5642bb
darektoa <darektoa@gmail.com>
2022-08-23 10:38:54 +0700

    Update: Import classname function of str helper in Text.Brand component

--- src/scripts/components/Text/Brand/Brand.jsx
+++ src/scripts/components/Text/Brand/Brand.jsx
@@ -1,12 +1,12 @@
 import CONFIG from '../../../config';
-import Str from '../../../utilities/string-helper';
+import { className as classname } from '../../../utilities/string-helper';
 
 const Brand = (props) => {
     const { children, className, value, ...attrs } = props;
 
     return (
         <span {...attrs}
-            className={Str.className('font-bold text-white', className)}
+            className={classname('font-bold text-white', className)}
             data-cy="header-title">
             { children || value || CONFIG.APP.NAME }      
         </span>
