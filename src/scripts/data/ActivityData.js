de72afa108194664d2934024a6df1097d5e08d96
darektoa <darektoa@gmail.com>
2022-08-24 06:42:51 +0700

    Add: Get method in activity data object

--- src/scripts/data/ActivityData.js
+++ src/scripts/data/ActivityData.js
@@ -2,6 +2,15 @@ import CONFIG from '../config';
 
 const ActivityData = {
     endpoint: `${CONFIG.API_BASE_URL}/activity-groups`,
+
+    async get() {
+        const options   = {};
+        const request   = new Request(this.endpoint, options);
+        const response  = await fetch(request);
+        const resJson   = await response.json();
+
+        return resJson;
+    },
 };
 
 
