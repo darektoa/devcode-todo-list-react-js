99bc07009ac91e83383b6a6706393d9742812c1d
darektoa <darektoa@gmail.com>
2022-08-24 21:26:51 +0700

    Add: Card.Activity component

---
+++ src/scripts/components/Card/Activity/Activity.jsx
@@ -0,0 +1,26 @@
+import {className as classname} from "../../../utilities/string-helper";
+import { Link } from "react-router-dom";
+import Icon from "../../Icon";
+
+const Activity = (props) => {
+    const { className, data, deleteHandle, ...attrs } = props;
+    const createdAt = (new Date(data.created_at)).toLocaleDateString('id-ID', { 
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+    });
+
+    return (
+        <Link {...attrs}
+            to={`/activity/${data.id}`}
+            className={classname("min-w-[12rem] aspect-square px-7 py-6 bg-white rounded-xl shadow-md flex flex-col justify-between", className)}>
+            <h3 className="font-bold text-lg" data-cy="activity-item-title">{data.title}</h3>
+            <div className="flex justify-between items-center">
+                <span className="text-sm text-[#888]" data-cy="activity-item-date">{ createdAt }</span>
+                <Icon name="trash-gray" className="w-6" onClick={deleteHandle} data-cy="activity-item-delete-button" />
+            </div>
+        </Link>
+    );
+}
+
+export default Activity;
--- src/scripts/components/Card/index.js
+++ src/scripts/components/Card/index.js
@@ -1,3 +1,6 @@
 import Activity from "./Activity";
+import Card from "./Card";
+
+Card.Activity = Activity;
 
 export default Card;
