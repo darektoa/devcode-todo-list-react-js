7ebb762ba091bf143e341463e446a41e4eb3116d
darektoa <darektoa@gmail.com>
2022-08-24 11:42:07 +0700

    Add: Create method in activity data object

--- src/scripts/data/ActivityData.js
+++ src/scripts/data/ActivityData.js
@@ -3,6 +3,17 @@ import CONFIG from '../config';
 const ActivityData = {
     endpoint: `${CONFIG.API_BASE_URL}/activity-groups`,
 
+    async create(data) {
+        const options   = {method: 'POST', body: JSON.stringify(data), headers: {
+            'Content-Type': 'application/json'
+        }};
+        const request   = new Request(this.endpoint, options);
+        const response  = await fetch(request);
+        const resJson   = await response.json();
+
+        return resJson;
+    },
+
     async get() {
         const options   = {};
         const request   = new Request(this.endpoint, options);
